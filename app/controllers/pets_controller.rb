7889ff3d025104729fbbe0ad4f15dde2d04e045b
duruchima <duruchima1@gmail.com>
2020-05-06 16:47:16 -0400

    renders json in index page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,6 @@
 class PetsController < ApplicationController
     def index
-        token = curl -d `grant_type=client_credentials&client_id=#{ENV["PETFINDER_KEY"]}&client_secret=#{ENV["PETFINDER_SECRET"]}` https://api.petfinder.com/v2/oauth2/token
+        @pets = Pet.all
+        render json:@pets
     end
 end
