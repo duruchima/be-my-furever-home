0b13dbfe37534d7cc96bfc3379f5d4c879005af6
duruchima <duruchima1@gmail.com>
2020-05-07 19:19:45 -0400

    added show logic to pet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    def create
+        @user= User.new(user_params)
+        @user.save
+        render json:@user
+    end
+
+
+
+    def user_params
+        params.require(:user).permit(:name, :username, :location)
+    end
 end
